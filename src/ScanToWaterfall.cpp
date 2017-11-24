3f052cd10322e1dd055875a2a0bd2a2891d849d0
olayasturias <olaya_93@hotmail.es>
2017-11-24 12:27:45 +0100

    reading pcl

--- src/ScanToWaterfall.cpp
+++ src/ScanToWaterfall.cpp
@@ -37,41 +37,24 @@ public:
   ros::NodeHandle n_;
   laser_geometry::LaserProjection projector_;                       /*!< provides the methods to transform from LaserScan to PointCloud */
   tf::TransformListener listener_;                                  /*!< used to transform the point cloud received to the desired frame */
-  message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub_;   /*!< LaserScan suscriber */
-  tf::MessageFilter<sensor_msgs::LaserScan> laser_notifier_;        /*!< used to execute the callback every time a LaserScan message is received */
-  ros::Publisher scan_pub_;                                         /*!< ros publisher used to publish the point cloud */
+  message_filters::Subscriber<sensor_msgs::PointCloud> pc_sub_;   /*!< LaserScan suscriber */
+  tf::MessageFilter<sensor_msgs::PointCloud> pc_notifier_;        /*!< used to execute the callback every time a LaserScan message is received */
+  ros::Publisher waterfall_pub_;                                         /*!< ros publisher used to publish the point cloud */
   pcl::visualization::CloudViewer viewer;                           /*!< used to configure the viewer */
 
-  //! Class constructor
-  /*! \param n_ ros Node handler
-   *  \param laser_sub_ LaserScan suscriber to topic indicated in console
-   *  \param laser_notifier tf frame selection
-   *  \param viewer viewer configuration
-   */
   ScanToWaterfall(ros::NodeHandle n) :
     n_(n),
-    laser_sub_(n_, topic, 10),
-    laser_notifier_(laser_sub_,listener_, "shaft", 10),
+    pc_sub_(n_, topic, 10),
+    pc_notifier_(pc_sub_,listener_, "shaft", 10),
     viewer("Profiling Viewer")
-  {/*
-      // Select if you want to change LaserScan topic
-      std::string input = "";
-      cout << "Do you want to change topic /g500/ProfilingSonar? (y/n): ";
-      getline(cin, input);
-      if (strcmp("y",input)==0 || strcmp("Y",input)==0)
-      {
-          laser_sub_(n_, input, 10);
-      }
-      else
-          std::cout << "Suscribing to /g500/ProfilingSonar" << std::endl;*/
-
+  {
       //Callback that is executed every time a LaserScan message is received
-    laser_notifier_.registerCallback(
+    pc_sub_.registerCallback(
       boost::bind(&ScanToWaterfall::scanCallback, this, _1));
-    laser_notifier_.setTolerance(ros::Duration(0.01));
+    pc_notifier_.setTolerance(ros::Duration(0.01));
 
     // Point cloud will be published in /ProfilingSonar_cloud
-    scan_pub_ = n_.advertise<sensor_msgs::PointCloud2>("/ProfilingSonar_cloud",1);
+    waterfall_pub_ = n_.advertise<sensor_msgs::PointCloud>("/ProfilingSonar_cloud",1);
   }
 
   //! Callback that is executed every time a LaserScan message is received
@@ -79,13 +62,25 @@ public:
    * and the LaserScan data to PointCloud. Then, the new PointCloud is concatenated with the previous received,
    * an finally, the complete Point Cloud is published.
    */
-  void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
+  void scanCallback (const sensor_msgs::PointCloud::ConstPtr& scan_in)
   {
-    sensor_msgs::PointCloud2 cloud;
+    sensor_msgs::PointCloud cloud;
+    pcl::PointCloud<pcl::PointXYZ>  aux_cloud;
     try
     {
-        projector_.transformLaserScanToPointCloud(
-          "shaft",*scan_in, cloud,listener_);
+        for (int i = 0; i < scan_in->points.size(); i++)
+        {
+                aux_cloud.x = scan_in->points[i].x;
+                aux_cloud.y = scan_in->points[i].y;
+                //x = scan_in->points[i].x;
+                //std::cout << x;
+                //y = scan_in->points[i].y;
+                //scan_in->points[i].z = i;
+                for (int k = 0; k< scan_in->points[i].size(); k++)
+                    aux_cloud.z = 0;
+
+        }
+
     }
     catch (tf::TransformException& e)
     {
@@ -94,19 +89,21 @@ public:
     }
 
     //Convert sensor_msgs PointCloud2 to pcl::PointCloud<pcl::pointxyz>
-    pcl::PointCloud<pcl::PointXYZ>  pcl_cloud, aux;
+    //pcl::PointCloud<pcl::PointXYZ>  pcl_cloud, aux;
                   //input | output//
-    pcl::fromROSMsg(cloud, pcl_cloud);
+    //pcl::fromROSMsg(cloud, pcl_cloud);
 
     //Concatenating total_cloud and pcl_cloud
-    total_cloud += pcl_cloud;
+    //total_cloud += pcl_cloud;
 
     //Representing the point cloud
-    pcl::PointCloud<pcl::PointXYZ>::Ptr draw_cloud (new pcl::PointCloud<pcl::PointXYZ>(total_cloud));
+    //pcl::PointCloud<pcl::PointXYZ>::Ptr draw_cloud (new pcl::PointCloud<pcl::PointXYZ>(total_cloud));
 
-    viewer.showCloud(draw_cloud);
+    //viewer.showCloud(draw_cloud);
+    //viewer.showCloud(scan_in);
 
-    scan_pub_.publish(cloud);
+
+    //waterfall_pub_.publish(cloud);
 
   }
 };
